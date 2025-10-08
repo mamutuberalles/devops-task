2d64af71e28fa09f525b01a29de47b259616b241
mamutuberalles <teglasak@gmail.com>
2025-10-08 15:46:36 +0200

    Cleaning branch

--- run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Assumes an existing virtualenv at .venv
-source .venv/bin/activate
-gunicorn --bind 0.0.0.0:8080 -w 2 helloapp.app:app
--- test.sh
+++
@@ -1,5 +0,0 @@
-#/bin/bash
-
-# Assumes an existing virtualenv at .venv
-source .venv/bin/activate
-python -m unittest helloapp.test
